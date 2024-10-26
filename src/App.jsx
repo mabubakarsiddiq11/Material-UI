8e339c8ab0725980161e141af28e03450fe81f9b
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2024-10-25 18:03:03 -0700

    add more button

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,20 @@ function App() {
     <>
       <h1>Hello Material UI</h1>
 
-     <div style={{display : 'flex ',justifyContent:'space-around'}}>
-     <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-     </div>
+      <div style={{ display: "flex ", justifyContent: "space-around" }}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </div>
+      <div style={{ display: "flex ", justifyContent: "space-around" ,marginTop : '20px'}}>
+        <Button color="secondary">Secondary</Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+      </div>
     </>
   );
 }
